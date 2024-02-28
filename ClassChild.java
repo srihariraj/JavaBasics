4d8cb3de01e60f73cf6f7f80c11e447e8e0f6ec3
srihariraj <srihariatom@gmail.com>
2024-02-28 11:55:10 +0530

    no changes

---
+++ ClassChild.java
@@ -0,0 +1,7 @@
+public class ClassChild {
+    public static float t = 3.14f;
+    public  double c = 2.997;
+    public  static void Childtest(){
+        System.out.println("Child program");
+    }
+}
---
+++ ClassParent.java
@@ -0,0 +1,4 @@
+public class ClassParent {
+    public static int a=100;
+    public static String x="Test";
+}
