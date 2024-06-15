d4ddb5b05687844c68eb7489d63d6750f5c539a9
srihariraj <srihariatom@gmail.com>
2024-06-16 04:26:50 +0530

    patterns

--- Basic Algorithms/StarPattern.java
+++ Basic Algorithms/StarPattern.java
@@ -2,23 +2,26 @@ import java.util.Scanner;
 
 public class StarPattern {
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int n = S.nextInt();
-        System.out.println("Enter the row");
-        int i = S.nextInt();
-        System.out.println("Enter the column");
-        int j = S.nextInt();
-        while (i > 0) {
-            while (j > 0) {
-                System.out.print("*");
-                j--;
+        Scanner inp = new Scanner(System.in);
+        int size = inp.nextInt();
+
+        PrintPattern(size);
+
+        inp.close();
+    }
+
+    public static void PrintPattern(int size) {
+        for (int i = 1; i <= size; i++) {// lowest to highest
+            System.out.print("Loop " + i + "\t");
+
+            for (int k = i; k < size; k++) {// current to highest
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i; j++) {// 0 to current
+                System.out.print("x");
             }
             System.out.println();
-            i--;
         }
-
-        S.close();
-
     }
 }
